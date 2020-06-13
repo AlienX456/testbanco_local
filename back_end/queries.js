901603e2c4fcc331c8a04abb697c231b2314c174
alienx456 <estebanelias27@gmail.com>
2020-06-13 10:01:21 -0500

    Create employee finished, ready to merge

--- back_end/queries.js
+++ back_end/queries.js
@@ -27,9 +27,21 @@ const getAllEmployee = (request, response) => {
     })
 }
 
+const createEmployee = (request, response) => {
+    const { id, fullname, boss, employeeFunction} = request.body
+  
+    pool.query("INSERT INTO employee_schema.employee (id,fullname,function,boss) VALUES ($1,$2,$3,$4)", [id, fullname, boss, employeeFunction], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Employee added`)
+    })
+  }
+
 
 
 module.exports = {
     getEmployee,
     getAllEmployee,
+    createEmployee
 }
