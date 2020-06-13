e93dde05176b4e905c93ec31d5db0415ceacfbfb
alienx456 <estebanelias27@gmail.com>
2020-06-13 09:46:45 -0500

    Added id field to manage efficiently PK to search and more

--- bd/init/init.sql
+++ bd/init/init.sql
@@ -1,8 +1,9 @@
 CREATE SCHEMA IF NOT EXISTS employee_schema;
 
 CREATE TABLE IF NOT EXISTS employee_schema.employee (
-	fullname VARCHAR NOT NULL PRIMARY KEY,
-    boss VARCHAR,
+    id INTEGER NOT NULL PRIMARY KEY CONSTRAINT positive_id CHECK (id > 0),
+	fullname VARCHAR NOT NULL,
+    boss INTEGER,
 	function VARCHAR NOT NULL,
-    FOREIGN KEY (boss) REFERENCES employee_schema.employee(fullname)
+    FOREIGN KEY (boss) REFERENCES employee_schema.employee(id)
 );
